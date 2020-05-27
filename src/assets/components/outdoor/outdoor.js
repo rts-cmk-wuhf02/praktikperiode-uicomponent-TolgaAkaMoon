18396a0f55f7b478bb069599fc689b93e5e2f8ef
TolgaAkaMoon <4160141@RTS-365.dk>
2020-05-27 13:15:48 +0200

    component finished

--- src/assets/components/outdoor/outdoor.js
+++ src/assets/components/outdoor/outdoor.js
@@ -10,11 +10,11 @@ client.getEntry('76cAgEMACO3El4X1S50DU5') // entry id
 .then((entry) => {
   client.getAssets('676qA6XWKLOR9gBLO3j9fw') // asset id
   .then((asset) => {
-      console.log(asset.items[0].fields.file.url)
+    console.log(asset.items)
       document.querySelector(".Outdoor__background").setAttribute("src", asset.items[4].fields.file.url)
-      document.querySelector(".Outdoor__smallimg").setAttribute("src", asset.items[0].fields.file.url)
-      document.querySelector(".Outdoor__smallimg").setAttribute("src", asset.items[0].fields.file.url)
-      document.querySelector(".Outdoor__smallimg").setAttribute("src", asset.items[0].fields.file.url)
+      document.querySelector(".Outdoor__smallimg1").setAttribute("src", asset.items[0].fields.file.url)
+      document.querySelector(".Outdoor__smallimg2").setAttribute("src", asset.items[1].fields.file.url)
+      document.querySelector(".Outdoor__smallimg3").setAttribute("src", asset.items[2].fields.file.url)
   }
   );
   document.querySelector(".Outdoor__title").innerHTML = entry.fields.title;
