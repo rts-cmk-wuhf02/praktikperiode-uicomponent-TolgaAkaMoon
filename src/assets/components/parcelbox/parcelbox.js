28b20e57b40273aa6ce3f3e7c1b587cef59fa4e5
TolgaAkaMoon <4160141@RTS-365.dk>
2020-05-28 09:40:43 +0200

    fix parcelbox js

--- src/assets/components/parcelbox/parcelbox.js
+++ src/assets/components/parcelbox/parcelbox.js
@@ -2,13 +2,13 @@ const contentful = require('contentful')
 
 const client = contentful.createClient({
   space: '3s5j27n9nypm', // contentful space_id
-  environment: 'master', // defaults to 'master' if not set
+  environment: 'master',
   accessToken: 'yR2gL3HeWPJLJS2umEzQG38R-N1ACaHaWq4_TVFO5F0' // contentful access token
 })
 
 client.getEntry('koq3QVILiLDXBNspfdlKp') // entry id
 .then((entry) => {
-  client.getAsset('5GoFws2M1OuSvabRXN1YTo') // asset id
+  client.getAsset('5GoFws2M1OuSvabRXN1YTo') // 1GjiePoOIw8gRVfGnRqNp3asset id
   .then((image) => {
     console.log(image)  
 document.querySelector(".Parcelbox__background").setAttribute("src", asset.fields.file.url)});
